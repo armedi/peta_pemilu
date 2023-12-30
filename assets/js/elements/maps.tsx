d756a38eb326e5fa0f8d318febdc8482e4f7c1c2
Armedi <armedi@outlook.com>
2023-12-30 21:52:27 +0700

    handle initial mount data

--- assets/js/elements/maps.tsx
+++ assets/js/elements/maps.tsx
@@ -1,10 +1,10 @@
 import "preact/debug";
 
 import { useSignal, useSignalEffect, type Signal } from "@preact/signals";
-import { type LatLngExpression, type Map as LeafletMap } from "leaflet";
+import { type LatLngLiteral, type Map as LeafletMap } from "leaflet";
 import type { ViewHook } from "phoenix_live_view";
 import register from "preact-custom-element";
-import { useRef } from "preact/hooks";
+import { useEffect, useRef } from "preact/hooks";
 import { GeoJSON, MapContainer, TileLayer, useMapEvents } from "react-leaflet";
 
 const tagName = "x-maps";
@@ -20,6 +20,7 @@ export const phxHooks = {
   [tagName]: {
     mounted(this: ViewHook) {
       liveViewHooks.set(this.el, this);
+      this.el.dispatchEvent(new Event("mounted"));
     },
     destroyed(this: ViewHook) {
       liveViewHooks.delete(this.el);
@@ -27,11 +28,11 @@ export const phxHooks = {
   },
 };
 
-function MapEvents(props: { center: Signal<LatLngExpression>; areas: Signal }) {
+function MapEvents(props: { center: Signal<LatLngLiteral>; areas: Signal }) {
   useMapEvents({
     click(e) {
       getLiveViewHook(e.originalEvent.target as HTMLElement).pushEvent(
-        "select_map_point",
+        "set_map_view",
         e.latlng,
         ({ data }) => {
           props.areas.value = data;
@@ -40,7 +41,7 @@ function MapEvents(props: { center: Signal<LatLngExpression>; areas: Signal }) {
     },
     zoom(e) {
       const map = e.target as LeafletMap;
-      getLiveViewHook(map.getContainer()).pushEvent("zoom_map", {
+      getLiveViewHook(map.getContainer()).pushEvent("set_map_view", {
         zoom: map.getZoom(),
       });
     },
@@ -52,33 +53,62 @@ function MapEvents(props: { center: Signal<LatLngExpression>; areas: Signal }) {
 function Maps(props: Record<string, string>) {
   const mapRef = useRef<LeafletMap>(null);
 
-  const center = useSignal<LatLngExpression>(JSON.parse(props.center));
+  const center = useSignal<LatLngLiteral>(JSON.parse(props.center));
   const zoom = useSignal<number>(JSON.parse(props.zoom));
   const areas = useSignal<any[]>([]);
 
   useSignalEffect(() => {
-    mapRef.current?.setView(center.value);
+    mapRef.current?.setView(center.value, zoom.value);
   });
 
+  useEffect(() => {
+    const element = mapRef.current?.getContainer().closest(tagName);
+
+    const handler = async () => {
+      if (window.location.pathname === "/" && "geolocation" in navigator) {
+        await new Promise((resolve) => {
+          navigator.geolocation.getCurrentPosition((position) => {
+            center.value = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+            zoom.value = 10;
+            resolve(void 0);
+          });
+        });
+      }
+
+      console.log(center.value, zoom.value)
+
+      getLiveViewHook(element as HTMLElement).pushEvent(
+        "set_map_view",
+        {
+          lat: center.value.lat,
+          lng: center.value.lng,
+          zoom: zoom.value,
+        },
+        (reply) => {
+          if (reply) {
+            areas.value = reply.data;
+          }
+        }
+      );
+    };
+
+    element?.addEventListener("mounted", handler);
+
+    return () => element?.removeEventListener("mounted", handler);
+  }, []);
+
   return (
-    <MapContainer
-      ref={mapRef}
-      center={center.value}
-      zoom={zoom.value}
-      className="h-full"
-    >
+    <MapContainer ref={mapRef} className="h-full">
       <MapEvents center={center} areas={areas} />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {areas.value.map((area, idx) => {
-        const color =
-          idx === 0
-            ? "red"
-            : idx === 1
-              ? "green"
-              : "blue"
+        const color = idx === 0 ? "red" : idx === 1 ? "green" : "blue";
 
         return (
           <GeoJSON
--- lib/peta_pemilu_web/live/index.ex
+++ lib/peta_pemilu_web/live/index.ex
@@ -3,9 +3,12 @@ defmodule PetaPemiluWeb.Live.Index do
   alias PetaPemilu.Area
   require Jason
 
+  @default_zoom 5.0
+  @default_lat 0.0
+  @default_lng 119.3
+
   defp dapil(assigns) do
     ~H"""
-    <%!-- <div class="mb-6 [&:nth-child(1)]:text-[#f8c7c0] [&:nth-child(2)]:text-[#cfbaa0] [&:nth-child(3)]:text-[#a49bb4]"> --%>
     <div class="mb-6 [&:nth-child(1)]:text-[#ea5d49] [&:nth-child(2)]:text-[#aa8456] [&:nth-child(3)]:text-[#706486]">
       <div>DAPIL <%= @area["jenis_dapil"] %>: <strong><%= @area["nama_dapil"] %></strong></div>
       <ul class="list-inside list-disc">
@@ -39,58 +42,68 @@ defmodule PetaPemiluWeb.Live.Index do
     """
   end
 
+  defp parse_map_view(map_view) do
+    map_view
+    |> String.trim_leading("@")
+    |> String.trim_trailing("z")
+    |> String.split(",")
+    |> Enum.map(fn x -> Float.parse(x) |> elem(0) end)
+    |> (&Enum.zip([:lat, :lng, :zoom], &1)).()
+  end
+
   def mount(
-        %{"map_view" => map_view},
+        %{"map_view" => map_view_string},
         _session,
         socket
       ) do
-    assigns =
-      map_view
-      |> String.trim_leading("@")
-      |> String.trim_trailing("z")
-      |> String.split(",")
-      |> Enum.map(fn x -> Float.parse(x) |> elem(0) end)
-      |> (&Enum.zip([:lat, :lng, :zoom], &1)).()
+    map_view = parse_map_view(map_view_string)
 
-    case assigns do
-      [_, _, zoom: _zoom] -> {:ok, assign(socket, assigns)}
-      [_, _] -> {:ok, assign(socket, assigns ++ [zoom: 5.0])}
-      _ -> {:ok, assign(socket, lat: 0.0, lng: 119.3, zoom: 5.0)}
+    case map_view do
+      [_, _, zoom: _zoom] -> {:ok, assign(socket, map_view)}
+      [_, _] -> {:ok, assign(socket, map_view ++ [zoom: @default_zoom])}
+      _ -> {:ok, assign(socket, lat: @default_lat, lng: @default_lng, zoom: @default_zoom)}
     end
   end
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, lat: 0.0, lng: 119.3, zoom: 5.0)}
+    {:ok, assign(socket, lat: @default_lat, lng: @default_lng, zoom: @default_zoom)}
   end
 
   def handle_params(_unsigned_params, _uri, socket) do
     {:noreply, socket}
   end
 
-  def handle_event("select_map_point", %{"lat" => lat, "lng" => lng}, socket) do
+  def handle_event("set_map_view", %{"lat" => lat, "lng" => lng, "zoom" => zoom}, socket) do
     rounded_lat = if is_float(lat), do: Float.round(lat, 6), else: lat
     rounded_lng = if is_float(lng), do: Float.round(lng, 6), else: lng
-    zoom = socket.assigns.zoom
+    zoom = zoom * 1.0
 
     {:ok, data} = Area.by_coordinate(lat, lng)
 
     {:reply, %{"data" => data},
      socket
-     |> assign(lat: lat, lng: lng, areas: Enum.map(data, &Map.drop(&1, ["geojson"])))
+     |> assign(
+       lat: lat,
+       lng: lng,
+       zoom: zoom,
+       areas: Enum.map(data, &Map.drop(&1, ["geojson"]))
+     )
      |> push_patch(to: "/@#{rounded_lat},#{rounded_lng},#{zoom}z")}
   end
 
-  def handle_event("zoom_map", %{"zoom" => zoom}, socket) do
-    lat = socket.assigns.lat
-    lng = socket.assigns.lng
-    zoom = zoom * 1.0
+  def handle_event("set_map_view", %{"lat" => lat, "lng" => lng}, socket) do
+    handle_event(
+      "set_map_view",
+      %{"lat" => lat, "lng" => lng, "zoom" => socket.assigns.zoom},
+      socket
+    )
+  end
 
-    rounded_lat = if is_float(lat), do: Float.round(lat, 6), else: lat
-    rounded_lng = if is_float(lng), do: Float.round(lng, 6), else: lng
-
-    {:noreply,
-     socket
-     |> assign(zoom: zoom)
-     |> push_patch(to: "/@#{rounded_lat},#{rounded_lng},#{zoom}z")}
+  def handle_event("set_map_view", %{"zoom" => zoom}, socket) do
+    handle_event(
+      "set_map_view",
+      %{"lat" => socket.assigns.lat, "lng" => socket.assigns.lng, "zoom" => zoom},
+      socket
+    )
   end
 end
