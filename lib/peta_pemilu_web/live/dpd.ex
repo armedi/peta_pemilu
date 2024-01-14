88d1230cdbc373c904dffb30edfa3919a6c49e30
Armedi <armedi@outlook.com>
2024-01-14 10:48:49 +0700

    list DPD candidates

--- lib/peta_pemilu_web/live/dpd.ex
+++ lib/peta_pemilu_web/live/dpd.ex
@@ -4,17 +4,44 @@ defmodule PetaPemiluWeb.Live.Dpd do
 
   def render(assigns) do
     ~H"""
-    <div>
-      <pre>
-      <%= Jason.encode!(assigns.candidates, pretty: true) %>
-      </pre>
+    <div class="pb-8">
+      <div
+        style={"background-image: url(#{static_path(@socket, "/images/wave.svg")})"}
+        class="bg-left-bottom bg-[length:auto_2rem] bg-repeat-x pb-8 mb-12 text-center"
+      >
+        <h1 class="bg-red-500 pt-6 px-8 text-white uppercase font-bold text-xl">
+          <span class="block">Surat Suara Pemilihan Umum</span>
+          <span class="block">Anggota Dewan Perwakilan Daerah</span>
+          <span class="block">Republik Indonesia</span>
+          <span class="block">Tahun 2024</span>
+        </h1>
+      </div>
+      <h2 class="text-center mb-12 font-bold text-xl uppercase">
+        <span class="block">Daerah Pemilihan</span>
+        <span class="block"><%= @dapil %></span>
+      </h2>
+      <div class="flex flex-wrap justify-center gap-4 mx-auto max-w-6xl">
+        <%= for candidate <- @candidates do %>
+          <div class="h-64 w-32 overflow-clip border flex flex-col border-gray-500 text-center">
+            <div class="p-1 font-bold text-lg"><%= candidate.nomor_urut %></div>
+            <img
+              src={candidate.foto}
+              loading="lazy"
+              alt={candidate.nama}
+              class="w-32 h-40 object-cover object-top"
+            />
+            <div style="text-wrap: balance" class="text-xs flex-1 flex justify-center items-center"><%= candidate.nama %></div>
+          </div>
+        <% end %>
+      </div>
     </div>
     """
   end
 
   def mount(%{"slug" => slug}, _session, socket) do
+    dapil = String.split(slug, "-") |> Enum.join(" ")
     candidates = PetaPemilu.Candidate.caleg_by_dapil(:dpd, slug)
 
-    {:ok, assign(socket, :candidates, candidates)}
+    {:ok, assign(socket, candidates: candidates, dapil: dapil)}
   end
 end
--- lib/peta_pemilu_web/live/dpr.ex
+++ lib/peta_pemilu_web/live/dpr.ex
@@ -4,17 +4,39 @@ defmodule PetaPemiluWeb.Live.Dpr do
 
   def render(assigns) do
     ~H"""
-    <div>
-      <pre>
-      <%= Jason.encode!(assigns.candidates, pretty: true) %>
-      </pre>
+    <div class="pb-8">
+      <div
+        style={"background-image: url(#{static_path(@socket, "/images/wave.svg")})"}
+        class="bg-left-bottom bg-[length:auto_2rem] bg-repeat-x pb-8 mb-12 text-center"
+      >
+        <h1 class="bg-red-500 pt-6 px-8 text-white uppercase font-bold text-xl">
+          <span class="block">Surat Suara Pemilihan Umum</span>
+          <span class="block">Anggota Dewan Perwakilan Rakyat</span>
+          <span class="block">Republik Indonesia</span>
+          <span class="block">Tahun 2024</span>
+        </h1>
+      </div>
+      <h2 class="text-center mb-12 font-bold text-xl uppercase">
+        <span class="block">Daerah Pemilihan</span>
+        <span class="block"><%= @dapil %></span>
+      </h2>
+      <div class="flex flex-wrap justify-center gap-12 mx-auto max-w-6xl">
+        <%= for candidate <- @candidates do %>
+          <div class="h-80 w-60 border border-black">
+            <%!-- <%= Jason.encode!(candidate, pretty: true) %> --%>
+          </div>
+          <%!-- <pre>
+        </pre> --%>
+        <% end %>
+      </div>
     </div>
     """
   end
 
   def mount(%{"slug" => slug}, _session, socket) do
+    dapil = String.split(slug, "-") |> Enum.join(" ")
     candidates = PetaPemilu.Candidate.caleg_by_dapil(:dpr, slug)
 
-    {:ok, assign(socket, :candidates, candidates)}
+    {:ok, assign(socket, candidates: candidates, dapil: dapil)}
   end
 end
