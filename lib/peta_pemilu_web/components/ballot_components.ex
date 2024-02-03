11d667ac70820aae50e554d8ad628a641cd17fdf
Armedi <armedi@outlook.com>
2024-02-03 21:48:10 +0700

    show party logo

--- lib/peta_pemilu_web/components/ballot_components.ex
+++ lib/peta_pemilu_web/components/ballot_components.ex
@@ -107,41 +107,37 @@ defmodule PetaPemiluWeb.BallotComponents do
     ~H"""
     <div class="flex flex-wrap justify-center gap-8 px-8 mx-auto">
       <%= for party <- @parties do %>
-        <div
-          style={"background-image: url('#{party.foto_partai}'); height: #{82 + (@rows * 37)}px"}
-          class="h-[452px] w-80 bg-center bg-cover bg-no-repeat border border-gray-500"
-        >
-          <div class="h-full bg-[rgba(255,255,255,0.975)]">
-            <h3 class="font-bold text-lg uppercase px-6 h-20 flex items-center gap-4 leading-5">
-              <span class="block text-4xl w-10 text-center"><%= party.nomor_urut_partai %></span>
-              <span class="block"><%= party.partai %></span>
-            </h3>
-            <ol class="[counter-reset:listCounter]">
-              <%= for c <- party.caleg do %>
-                <li class="[&:first-child]:border-t border-b border-gray-500 border-spacing-0 text-sm [counter-increment:listCounter] relative">
-                  <%= if c["id"] do %>
-                    <a
-                      href={
-                        ~p"/caleg/#{case assigns.jenis_dapil do
-                          :dpr -> "dpr"
-                          :dprd_prov -> "dprd-prov"
-                          :dprd_kabko -> "dprd-kabko"
-                        end}/#{assigns.dapil_slug}/#{c["id"]}"
-                      }
-                      target="_blank"
-                      class="block w-full py-2 px-4 whitespace-nowrap truncate before:content-[counter(listCounter)] before:text-right before:inline-block before:w-4 before:mr-4 after:content-['.'] after:absolute after:left-8"
-                    >
-                      <%= c["nama"] %>
-                    </a>
-                  <% else %>
-                    <span class="block w-full text-gray-400 py-2 px-4 whitespace-nowrap truncate before:content-[counter(listCounter)] before:text-right before:inline-block before:w-4 before:mr-4 after:content-['.'] after:absolute after:left-8">
-                      <%= c["nama"] %>
-                    </span>
-                  <% end %>
-                </li>
-              <% end %>
-            </ol>
-          </div>
+        <div style={"height: #{82 + (@rows * 37)}px"} class="h-[452px] w-80 border border-gray-500">
+          <h3 class="font-bold uppercase px-6 h-20 flex items-center gap-4 leading-5">
+            <span class="block text-4xl w-10 text-center"><%= party.nomor_urut_partai %></span>
+            <span class="block flex-1"><%= party.partai %></span>
+            <img class="block w-10 h-10 object-contain" src={party.foto_partai} />
+          </h3>
+          <ol class="[counter-reset:listCounter]">
+            <%= for c <- party.caleg do %>
+              <li class="[&:first-child]:border-t border-b border-gray-500 border-spacing-0 text-sm [counter-increment:listCounter] relative">
+                <%= if c["id"] do %>
+                  <a
+                    href={
+                      ~p"/caleg/#{case assigns.jenis_dapil do
+                        :dpr -> "dpr"
+                        :dprd_prov -> "dprd-prov"
+                        :dprd_kabko -> "dprd-kabko"
+                      end}/#{assigns.dapil_slug}/#{c["id"]}"
+                    }
+                    target="_blank"
+                    class="block w-full py-2 px-4 whitespace-nowrap truncate before:content-[counter(listCounter)] before:text-right before:inline-block before:w-4 before:mr-4 after:content-['.'] after:absolute after:left-8"
+                  >
+                    <%= c["nama"] %>
+                  </a>
+                <% else %>
+                  <span class="block w-full text-gray-400 py-2 px-4 whitespace-nowrap truncate before:content-[counter(listCounter)] before:text-right before:inline-block before:w-4 before:mr-4 after:content-['.'] after:absolute after:left-8">
+                    <%= c["nama"] %>
+                  </span>
+                <% end %>
+              </li>
+            <% end %>
+          </ol>
         </div>
       <% end %>
     </div>
