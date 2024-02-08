7c677c55f880e52175f1e5c118cce3d469a815ef
Armedi <armedi@outlook.com>
2024-02-08 17:29:15 +0700

    create page for dpr and dprd candidate profile

--- lib/peta_pemilu_web/components/profile_components.ex
+++ lib/peta_pemilu_web/components/profile_components.ex
@@ -15,217 +15,124 @@ defmodule PetaPemiluWeb.ProfileComponents do
 
   defp dpr_dprd_profile(assigns) do
     ~H"""
-    <div>
-      <div>
-        <div>
-          <h4>PROFIL CALON</h4>
-        </div>
-        <div>
-          <table>
-            <tbody>
-              <tr>
-                <td width="50">
-                  <img
-                    src={"https://infopemilu.kpu.go.id/#{@candidate["logoPartai"]}"}
-                    alt={@candidate["namaPartai"]}
-                    width="40px"
-                  />
-                </td>
-                <td>
-                  <h5><%= @candidate["namaPartai"] %></h5>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div>
-          <i>Nomor urut</i>
-          <h3><%= @candidate["nomorUrut"] %></h3>
+    <main class="max-w-screen-md mx-auto my-8">
+      <div class="flex mb-12">
+        <div class="flex-1 flex flex-col justify-end relative">
+          <img
+            src={"https://infopemilu.kpu.go.id/#{@candidate["logoPartai"]}"}
+            alt={@candidate["namaPartai"]}
+            class="h-16 w-16 object-contain absolute top-0 right-3"
+          />
 
-          <img src={@candidate["pasFoto"]} alt={@candidate["nama"]} width="200px" />
-
-          <i>Nama dapil</i>
-          <h5><%= @candidate["namaDapil"] %></h5>
+          <h1 class="inline-flex min-w-0 items-end font-bold">
+            <span class="inline-flex justify-center items-center h-24 w-16 text-4xl bg-gray-600 text-white rounded-l">
+              <%= @candidate["nomorUrut"] %>
+            </span>
+            <span class="flex-1 inline-flex items-center px-6 py-4 border border-r-0 border-gray-400 text-2xl">
+              <%= @candidate["nama"] %>
+            </span>
+          </h1>
         </div>
-        <div>
-          <table>
-            <tbody>
-              <tr>
-                <td><strong>Nama Lengkap:</strong></td>
-                <td><%= @candidate["nama"] %></td>
-              </tr>
-              <tr>
-                <td><strong>Tempat Lahir:</strong></td>
-                <td><%= @candidate["tempatLahir"] %></td>
-              </tr>
-              <tr>
-                <td><strong>Usia:</strong></td>
-                <td><%= @candidate["usia"] %>></td>
-              </tr>
-              <tr>
-                <td><strong>Jenis Kelamin:</strong></td>
-                <td><%= @candidate["jenisKelamin"] %></td>
-              </tr>
-              <tr>
-                <td><strong>Agama:</strong></td>
-                <td><%= @candidate["agama"] %></td>
-              </tr>
-              <tr>
-                <td><strong>Status Disabilitas:</strong></td>
-                <td><%= @candidate["statusDisabilitas"] %></td>
-              </tr>
-            </tbody>
-          </table>
+
+        <img
+          src={@candidate["pasFoto"]}
+          alt={@candidate["nama"]}
+          class="w-32 aspect-[2/3] object-cover rounded-lg rounded-bl-none"
+        />
+      </div>
+
+      <%= unless is_nil(@candidate["programUsulan"]) do %>
+        <div class="mb-12">
+          <h3 class="text-lg font-semibold mb-2">Program Usulan</h3>
+          <ul>
+            <%= for program <- @candidate["programUsulan"] do %>
+              <li class="whitespace-pre-line">
+                <%= program %>
+              </li>
+            <% end %>
+          </ul>
         </div>
-      </div>
-    </div>
+      <% end %>
 
-    <div>
-      <h3>PEKERJAAN</h3>
-      <p><%= @candidate["pekerjaan"] %></p>
-    </div>
-    <%= unless is_nil(@candidate["riwayatPekerjaan"]) do %>
-      <div>
-        <h3>RIWAYAT PEKERJAAN</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Nama Perusahaan / Lembaga</th>
-              <th>Jabatan</th>
-              <th>Tahun Masuk</th>
-              <th>Tahun Keluar</th>
-            </tr>
-          </thead>
-          <tbody>
-            <%= for job <- @candidate["riwayatPekerjaan"] do %>
-              <tr>
-                <td><%= job["namaPerusahaanLembaga"] %></td>
-                <td><%= job["jabatan"] %></td>
-                <td><%= job["tahunMasuk"] %></td>
-                <td><%= job["tahunKeluar"] %></td>
-              </tr>
-            <% end %>
-          </tbody>
-        </table>
+      <div class="mb-12">
+        <h3 class="text-lg font-semibold mb-2">Pekerjaan</h3>
+        <p><%= @candidate["pekerjaan"] %></p>
       </div>
-    <% end %>
 
-    <div>
-      <h3>STATUS HUKUM</h3>
-      <p><%= @candidate["statusHukum"] %></p>
-    </div>
-    <%= unless is_nil(@candidate["riwayatPendidikan"]) do %>
-      <div>
-        <h3>RIWAYAT PENDIDIKAN</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Jenjang Pendidikan</th>
-              <th>Nama Institusi</th>
-              <th>Tahun Masuk</th>
-              <th>Tahun Keluar</th>
-            </tr>
-          </thead>
-          <tbody>
-            <%= for education <- @candidate["riwayatPendidikan"] do %>
-              <tr>
-                <td><%= education["jenjangPendidikan"] %></td>
-                <td><%= education["namaInstitusi"] %></td>
-                <td><%= education["tahunMasuk"] %></td>
-                <td><%= education["tahunKeluar"] %></td>
-              </tr>
-            <% end %>
-          </tbody>
-        </table>
-      </div>
-    <% end %>
-    <%= unless is_nil(@candidate["riwayatKursusDiklat"]) do %>
-      <div>
-        <h3>RIWAYAT KURSUS DAN DIKLAT</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Nama Kursus</th>
-              <th>lembaga penyelenggara</th>
-              <th>nomor sertifikat</th>
-              <th>Tahun Masuk</th>
-              <th>Tahun Keluar</th>
-            </tr>
-          </thead>
-          <tbody>
-            <%= for course <- @candidate["riwayatKursusDiklat"] do %>
-              <tr>
-                <td><%= course["namaKursus"] %></td>
-                <td><%= course["lembagaPenyelenggara"] %></td>
-                <td><%= course["nomorSertifikat"] %></td>
-                <td><%= course["tahunMasuk"] %></td>
-                <td><%= course["tahunKeluar"] %></td>
-              </tr>
-            <% end %>
-          </tbody>
-        </table>
-      </div>
-    <% end %>
-    <%= unless is_nil(@candidate["riwayatOrganisasi"]) do %>
-      <div>
-        <h3>RIWAYAT ORGANISASI</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Nama Organisasi</th>
-              <th>Jabatan</th>
-              <th>Tahun Masuk</th>
-              <th>Tahun Keluar</th>
-            </tr>
-          </thead>
-          <tbody>
-            <%= for org <- @candidate["riwayatOrganisasi"] do %>
-              <tr>
-                <td><%= org["namaOrganisasi"] %></td>
-                <td><%= org["jabatan"] %></td>
-                <td><%= org["tahunMasuk"] %></td>
-                <td><%= org["tahunKeluar"] %></td>
-              </tr>
-            <% end %>
-          </tbody>
-        </table>
-      </div>
-    <% end %>
-    <%= unless is_nil(@candidate["riwayatPenghargaan"]) do %>
-      <div>
-        <h3>RIWAYAT PENGHARGAAN</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Nama Penghargaan</th>
-              <th>Lembaga</th>
-              <th>Tahun</th>
-            </tr>
-          </thead>
-          <tbody>
-            <%= for prize <- @candidate["riwayatPenghargaan"] do %>
-              <tr>
-                <td><%= prize["namaPenghargaan"] %></td>
-                <td><%= prize["lembaga"] %></td>
-                <td><%= prize["tahun"] %></td>
-              </tr>
-            <% end %>
-          </tbody>
-        </table>
-      </div>
-    <% end %>
-    <%= unless is_nil(@candidate["programUsulan"]) do %>
-      <div>
-        <h3>PROGRAM USULAN</h3>
-        <ul>
-          <%= for program <- @candidate["programUsulan"] do %>
-            <li>
-              <%= program %>
-            </li>
+      <%= unless is_nil(@candidate["riwayatPekerjaan"]) do %>
+        <div class="mb-12">
+          <h3 class="text-lg font-semibold mb-2">Riwayat Pekerjaan</h3>
+          <%= for job <- Enum.reverse(@candidate["riwayatPekerjaan"]) do %>
+            <div class="mb-2">
+              <h4 class="font-semibold"><%= job["namaPerusahaanLembaga"] %></h4>
+              <div><%= job["jabatan"] %></div>
+              <div class="text-sm text-gray-400">
+                <%= job["tahunMasuk"] %> - <%= job["tahunKeluar"] %>
+              </div>
+            </div>
           <% end %>
-        </ul>
-      </div>
-    <% end %>
+        </div>
+      <% end %>
+
+      <%= unless is_nil(@candidate["riwayatPendidikan"]) do %>
+        <div class="mb-12">
+          <h3 class="text-lg font-semibold mb-2">Riwayat Pendidikan</h3>
+          <%= for education <- Enum.reverse(@candidate["riwayatPendidikan"]) do %>
+            <div class="mb-2">
+              <h4 class="font-semibold"><%= education["namaInstitusi"] %></h4>
+              <div><%= education["jenjangPendidikan"] %></div>
+              <div class="text-sm text-gray-400">
+                <%= education["tahunMasuk"] %> - <%= education["tahunKeluar"] %>
+              </div>
+            </div>
+          <% end %>
+        </div>
+      <% end %>
+
+      <%= unless is_nil(@candidate["riwayatKursusDiklat"]) do %>
+        <div class="mb-12">
+          <h3 class="text-lg font-semibold mb-2">Riwayat Kursus dan Diklat</h3>
+          <%= for course <- Enum.reverse(@candidate["riwayatKursusDiklat"]) do %>
+            <div class="mb-2">
+              <h4 class="font-semibold"><%= course["namaKursus"] %></h4>
+              <div><%= course["lembagaPenyelenggara"] %></div>
+              <div>Nomor Sertifikat: <%= course["nomorSertifikat"] %></div>
+              <div class="text-sm text-gray-400">
+                <%= course["tahunMasuk"] %> - <%= course["tahunKeluar"] %>
+              </div>
+            </div>
+          <% end %>
+        </div>
+      <% end %>
+
+      <%= unless is_nil(@candidate["riwayatOrganisasi"]) do %>
+        <div class="mb-12">
+          <h3 class="text-lg font-semibold mb-2">Riwayat Organisasi</h3>
+          <%= for org <- Enum.reverse(@candidate["riwayatOrganisasi"]) do %>
+            <div class="mb-2">
+              <h4 class="font-semibold"><%= org["namaOrganisasi"] %></h4>
+              <div><%= org["jabatan"] %></div>
+              <div class="text-sm text-gray-400">
+                <%= org["tahunMasuk"] %> - <%= org["tahunKeluar"] %>
+              </div>
+            </div>
+          <% end %>
+        </div>
+      <% end %>
+
+      <%= unless is_nil(@candidate["riwayatPenghargaan"]) do %>
+        <div class="mb-12">
+          <h3 class="text-lg font-semibold mb-2">Riwayat Penghargaan</h3>
+          <%= for prize <- Enum.reverse(@candidate["riwayatPenghargaan"]) do %>
+            <div class="mb-2">
+              <h4 class="font-semibold"><%= prize["namaPenghargaan"] %></h4>
+              <div><%= prize["lembaga"] %></div>
+              <div class="text-sm text-gray-400"><%= prize["tahun"] %></div>
+            </div>
+          <% end %>
+        </div>
+      <% end %>
+    </main>
     """
   end
 end
