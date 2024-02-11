6fbef84c11152167ea2c0ab74d96e7a3e52f94fc
Armedi <armedi@outlook.com>
2024-02-11 08:27:33 +0700

    history.scrollRestoration = "auto"

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,3 +48,5 @@ liveSocket.connect();
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket;
+
+history.scrollRestoration = "auto";
